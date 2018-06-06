1296eaf35c6a677bee905777967b239c26398939
komsan <t_kub@windowslive.com>
2018-06-06 10:41:01 +0700

    Initial commit

---
+++ app/src/main/java/com/tod/hitachi/com/todshop/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tod.hitachi.com.todshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
