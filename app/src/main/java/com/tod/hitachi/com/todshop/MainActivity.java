8abce1bdbeb585b4aeaf86667eedcbf906dbc28a
komsan <t_kub@windowslive.com>
2018-06-06 11:47:41 +0700

    Create Main fragment suscceess

--- app/src/main/java/com/tod/hitachi/com/todshop/MainActivity.java
+++ app/src/main/java/com/tod/hitachi/com/todshop/MainActivity.java
@@ -3,11 +3,26 @@ package com.tod.hitachi.com.todshop;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.tod.hitachi.com.todshop.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment())
+                    .commit();
+
+
+        }
+
+
+
+    } // Main Method
+} // Main Class
